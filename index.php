203b599a617fed4f4c8d3b5c5024630fefd49650
Bernath Kylian <bernath.kylian.pro@gmail.com>
2017-06-11 14:05:46 +0200

    Correction du titre

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
   
   <meta http-equiv="Content-Type" content="text/html" charset="UTF-8">
   
-   <title>Le Stock - Home</title>
+   <title>Le Stock</title>
     
   
 <meta property="og:title" content="Le Stock" />
